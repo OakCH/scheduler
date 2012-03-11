fe92e7da3959e231b98ca9aa2ae1cc3a2f6f0155
saasbook <saasbook@saasbook.(none)>
2012-03-11 14:33:44 -0700

    Added a redirect route from the root to the login page.

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Scheduler::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => 'accounts#login'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
