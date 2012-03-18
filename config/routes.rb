0bde502625e6fc442d682add1d71380456337744
David Jimenez <davidejimenezp@berkeley.edu>
2012-03-18 13:22:30 -0700

    calendar and javascript in place

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Scheduler::Application.routes.draw do
+  match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
   
