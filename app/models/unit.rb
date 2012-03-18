60e69fd2303e63be13b6f5ed27e9f125369281ae
Sheel Choksi <sheelchoksi@berkeley.edu>
2012-03-18 11:47:00 -0700

    Capitalizing shift names

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,11 +1,11 @@
 class Unit < ActiveRecord::Base
   has_many :nurses
   has_many :vacation_days
-
+  
   validates_uniqueness_of :name
-
+  
   def self.shifts
-    ['days', 'pm\'s', 'nights']
+    ['Days', 'PMs', 'Nights']
   end
   
 end
