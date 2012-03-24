c68d906cc66df4950a256a85fa8494e0af1fbfd6
Andrew Nguyen <andrewnguyen@berkeley.edu>
2012-03-23 22:58:21 -0700

    got rid of FactoryGirl.find

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl'
-FactoryGirl.find_definitions
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
