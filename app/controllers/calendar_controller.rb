2d183d092878904ed11a47efea149fb57cb216f9
David Jimenez <davidejimenezp@berkeley.edu>
2012-03-24 19:57:06 -0700

    now nurses can save vacation days

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,14 +6,41 @@ class CalendarController < ApplicationController
 
     @shown_month = Date.civil(@year, @month)
 
-    nurse_id = params[:nurse_id]
-    @event_strips = Event.event_strips_for_month(@shown_month, :include => :nurse, :conditions => 'nurse_id = '+nurse_id)
+    nurse = Nurse.find(params[:nurse_id])
+
+    ids = Nurse.get_nurse_ids_shift_unit_id(nurse.shift, nurse.unit_id)
+
+    if ids
+      @event_strips = Event.event_strips_for_month(@shown_month, :include => :nurse, :conditions => 'nurse_id in '+ ids)
+    else
+      @event_strips = Event.event_strips_for_month(@shown_month)
+    end
+
   end
 
   def new 
+    @nurse_id = params[:nurse_id]
+  end
+
+  def create
+    nurse = Nurse.find(params[:nurse_id])
+    event = Event.new(params[:event])
+    event.name = nurse.name
+    nurse.events << event
+    nurse.save!
+    flash[:notice] = 'You successfully scheduled your vaction'
+    redirect_to nurse_calendar_index 
   end
 
   def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update_attributes!(params[:event])
+    flash[:notice] = 'You successfully scheduled your vaction'
+    redirect_to nurse_calendar_index 
   end
   
 end
--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -8,6 +8,22 @@ class Nurse < ActiveRecord::Base
     return Nurse.find_by_id(id)
   end
 
+  def self.get_nurse_ids_shift_unit_id(shift, unit_id)
+    ids = Nurse.find( :all, 
+                      :joins => "inner join events on events.nurse_id = nurses.id", 
+                      :select => 'nurses.id', 
+                      :conditions => "nurses.shift = '#{shift}' and nurses.unit_id = #{unit_id}")
+    if ids.length == 0
+      return nil
+    end
+    ids_str = '('
+    ids.each do |n|
+      ids_str += n.id.to_s
+      ids_str += ','
+    end
+    return ids_str[0..-2] + ')'
+  end
+
   def add_event(start_at, end_at)
     new_event = Event.create(:name => :name, :start_at => start_at, :end_at => end_at)
     self.events << new_event
