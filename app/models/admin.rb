b6439c2d7fbfa47dc49f2d67fb8f11abd8a164d2
Helen Weng <saasbook@saasbook.(none)>
2012-03-25 12:19:04 -0700

    Added Admin.create_event and Admin.delete_event with passing rspec.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,16 @@
 class Admin < ActiveRecord::Base
   validates_presence_of :name
+  def self.add_event(start_at,end_at,nurse_id)
+    nurse = Nurse.find_by_id(nurse_id)
+    event = Event.create!(:name=>nurse.name,:start_at=>start_at,:end_at=>end_at,:nurse_id=>nurse_id) 
+    if not event
+      return false
+    end
+  end
+
+  def self.delete_event(event)
+    event.destroy
+  end
+
 end
+
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,14 +1,46 @@
 require 'spec_helper'
 
 describe Admin do
+  before(:each) do
+    @admin = FactoryGirl.create(:admin)
+    @nurse = FactoryGirl.create(:nurse)
+    @start_at = '03/03/2012'
+    @end_at = '03/04/2012'
+  end
   describe 'Admin should be able to' do
     describe 'do CRUD on events' do
-      it 'should create an event'
-      it 'should delete an event'
-      it 'should update an event'
+      it 'should create an event for a specific nurse' do
+        start_at = '03/03/2012'
+        end_at = '03/04/2012'
+        Admin.add_event(@start_at, @end_at, @nurse.id)
+        events = Event.find(:all, :conditions => {:nurse_id => @nurse.id})
+        events.length.should==1
+        new_event = events[0]
+        new_event.start_at.should == @start_at
+        new_event.end_at.should == @end_at
+        new_event.nurse_id.should == @nurse.id
+        #what's the point of having Event.name when we already have nurse_id
+        new_event.name.should == @nurse.name
+      end
+      it 'should delete an event' do
+        new_event = Event.create!(:name => @nurse.name, :start_at => @start_at,
+                                  :end_at => @end_at, :nurse_id => @nurse.id)
+        new_event.should_not == nil
+        events = Event.find(:all, :conditions => {:id => new_event.id})
+        events.length.should==1
+        @nurse.events.length.should==1
+        Admin.delete_event(events[0])
+        events = Event.find(:all, :conditions => {:id => new_event.id})
+        events.length.should==0
+        nurse_ex = Nurse.find_by_id(@nurse.id)
+        nurse_ex.events.length.should==0
+      end
+      it 'should update an event' do
+      end
     end
     describe 'view events for one month' do
-      it 'should retrieve events for one month for one shift and one unit'
+      it 'should retrieve events for one month for one shift and one unit' do
+      end
     end
   end
 
