a8ac141e0c7b0fa45588c70b8ac3b613aba2e90f
Helen Weng <saasbook@saasbook.(none)>
2012-03-25 20:03:38 -0700

    Fixed admin spec to only show per month.

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,32 +1,39 @@
 require 'spec_helper'
+require 'date'
 
 describe Admin do
   before(:each) do
     @admin = FactoryGirl.create(:admin)
     @nurse = FactoryGirl.create(:nurse)
     @start_at = '03/03/2012'
-    @end_at = '03/04/2012'
+    @end_at = '11/11/2012'
   end
   describe 'view events for one month' do
     before(:each) do
       @unit3 = FactoryGirl.create(:unit)
       @unit2 = FactoryGirl.create(:unit)
-      nurses = [FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'Days')]
-      nurses << FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'Days')
-      nurses << FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'PMs')
-      nurses << FactoryGirl.create(:nurse, :unit=>@unit2, :shift=>'Days')
-      for nurse in nurses
-        event = Event.create!(:name => nurse.name, :start_at => @start_at, :end_at => @end_at, :nurse_id => nurse.id)
+      nurses = []
+      nurses << [FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'Days'),DateTime.new(2012,3,3,0,0,0)]
+      nurses << [FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'Days'),DateTime.new(2012,3,5,0,0,0)]
+      nurses << [FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'PMs'),DateTime.new(2012,3,5,0,0,0)]
+      nurses << [FactoryGirl.create(:nurse, :unit=>@unit2, :shift=>'Days'),DateTime.new(2012,3,5,0,0,0)]
+      nurses << [FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'Days'),DateTime.new(2012,5,5,0,0,0)]
+      nurses << [FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'Days'),DateTime.new(2011,5,5,0,0,0)]
+      for nurse,start_date in nurses
+        event = Event.create!(:name => nurse.name, :start_at => start_date, :end_at => @end_at, :nurse_id => nurse.id)
       end
     end
     it 'should show events of nurses constrainted by shift and unit with 1+ nurses' do
-      Admin.show_events_for_month({:unit_id=>@unit3.id, :shift=>'Days'}).length.should == 2
+      Admin.show_events_for_month(3,2012,{:unit_id=>@unit3.id, :shift=>'Days'}).length.should == 2
     end
     it 'should show events of nurses constrained by shift and unit with 1 nurse' do
-      Admin.show_events_for_month({:unit_id=>@unit2.id, :shift=>'Days'}).length.should == 1
+      Admin.show_events_for_month(3,2012,{:unit_id=>@unit2.id, :shift=>'Days'}).length.should == 1
     end
     it 'should show events of nurses constrained by unit only' do
-      Admin.show_events_for_month({:unit_id=>@unit3.id}).length.should == 3
+      Admin.show_events_for_month(3,2012,{:unit_id=>@unit3.id}).length.should == 3
+    end
+    it 'should not show events in other months and years' do
+      Admin.show_events_for_month(5,2011,{:unit_id=>@unit3.id}).length.should == 1
     end
   end
 end
