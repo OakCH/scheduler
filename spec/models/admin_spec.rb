7da13c4796ccea8e74a971e5b610e350dd54eb63
Helen Weng <saasbook@saasbook.(none)>
2012-03-25 17:51:52 -0700

    Fixed factory_girl parameters.

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -9,24 +9,24 @@ describe Admin do
   end
   describe 'view events for one month' do
     before(:each) do
-      unit3 = FactoryGirl.create(:unit, :id=>3)
-      unit2 = FactoryGirl.create(:unit, :id=>2)
-      nurses = [FactoryGirl.create(:nurse, :unit_id=>3, :shift=>'Days')]
-      nurses << FactoryGirl.create(:nurse, :unit_id=>3, :shift=>'Days')
-      nurses << FactoryGirl.create(:nurse, :unit_id=>3, :shift=>'PMs')
-      nurses << FactoryGirl.create(:nurse, :unit_id=>2, :shift=>'Days')
+      @unit3 = FactoryGirl.create(:unit)
+      @unit2 = FactoryGirl.create(:unit)
+      nurses = [FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'Days')]
+      nurses << FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'Days')
+      nurses << FactoryGirl.create(:nurse, :unit=>@unit3, :shift=>'PMs')
+      nurses << FactoryGirl.create(:nurse, :unit=>@unit2, :shift=>'Days')
       for nurse in nurses
         event = Event.create!(:name => nurse.name, :start_at => @start_at, :end_at => @end_at, :nurse_id => nurse.id)
       end
     end
     it 'should show events of nurses constrainted by shift and unit with 1+ nurses' do
-      Admin.show_events_for_month({:unit_id=>3, :shift=>'Days'}).length.should == 2
+      Admin.show_events_for_month({:unit_id=>@unit3.id, :shift=>'Days'}).length.should == 2
     end
     it 'should show events of nurses constrained by shift and unit with 1 nurse' do
-      Admin.show_events_for_month({:unit_id=>2, :shift=>'Days'}).length.should == 1
+      Admin.show_events_for_month({:unit_id=>@unit2.id, :shift=>'Days'}).length.should == 1
     end
     it 'should show events of nurses constrained by unit only' do
-      Admin.show_events_for_month({:unit_id=>3}).length.should == 3
+      Admin.show_events_for_month({:unit_id=>@unit3.id}).length.should == 3
     end
   end
 end
