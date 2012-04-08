785123446c1767af0580596b5606bd97013a3b5b
Christine Lu <christinelu.08@gmail.com>
2012-04-08 12:22:48 -0700

    admin_view_calendar cuke up to step where we check if vacation is in calendar

---
+++ features/step_definitions/calendar_steps.rb
@@ -0,0 +1,23 @@
+Given /the following vacations exist/ do |vacations_table|
+  vacations_table.hashes.each do |vacation|
+    vacation[:nurse_id] = Nurse.find_by_name(vacation[:name])
+    FactoryGirl.create(:event, vacation)
+  end
+end
+
+Given /the following nurses exist/ do |nurses_table|
+  nurses_table.hashes.each do |nurse_params|
+    unit = Unit.find_by_name(nurse_params[:unit])
+    unit = Unit.create!(:name => nurse_params[:unit]) if !unit
+    nurse_params[:unit] = nil
+    nurse_params[:unit_id] = unit.id
+    FactoryGirl.create(:nurse, nurse_params)
+  end
+end
+
+Then /^(?:I )?should see the vacation belonging to "([^"]*)" from "([^"]*)" to "([^"]*)"$/ do |nurse, start_date, end_date|
+  parsed_start = DateTime.parse(start_date).to_time
+  parsed_end = DateTime.parse(end_date).to_time
+  event = Event.find_by_name_and_start_at_and_end_at(nurse, parsed_start, parsed_end)
+  page.find("*[data-event-id='#{event.id}']")
+end
