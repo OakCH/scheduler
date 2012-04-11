7a8b4531353d73d14ee32d1ac2a2107d07630952
Sheel Choksi <sheelchoksi@berkeley.edu>
2012-04-10 22:35:12 -0700

    Removing uneeded devise modules from model and database migration

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,13 +1,12 @@
 require 'date'
 
 class Admin < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+  
+  devise :database_authenticatable, :recoverable, :rememberable 
+  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
   validates_presence_of :name
   
   def self.show_events_for_month(month,year,selectors)
@@ -16,7 +15,7 @@ class Admin < ActiveRecord::Base
     start_month = DateTime.new(year,month,1,0,0,0)
     end_month = start_month.next_month.prev_day
     if shift
-      return Unit.where(:id=>unit_id).joins({:nurses => [:events]}).where('nurses.shift = ? AND 
+      return Unit.where(:id=>unit_id).joins({:nurses => [:events]}).where('nurses.shift = ? AND
           events.start_at > ? AND events.start_at < ?', shift, start_month, end_month)
     else
       return Unit.find_by_id(unit_id).events.where('events.start_at > ? AND events.start_at < ?',
--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -1,12 +1,10 @@
 class Nurse < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :recoverable, :rememberable
   
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-
+  
   has_many :events
   belongs_to :unit
   
