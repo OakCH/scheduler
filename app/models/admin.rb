3cbac3cd6351248e50f9a88fe6ddae7430b1d501
Sheel Choksi <sheelchoksi@berkeley.edu>
2012-04-11 01:56:26 -0700

    Adding attr_accessible params for admin and nurse

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,7 +5,7 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable 
   
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
   validates_presence_of :name
   validates_uniqueness_of :email
--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -1,9 +1,10 @@
 class Nurse < ActiveRecord::Base
-
+  
   devise :database_authenticatable, :recoverable, :rememberable
   
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+  :seniority, :shift, :unit_id, :num_weeks_off, :name, :years_worked, :unit
   
   has_many :events
   belongs_to :unit
