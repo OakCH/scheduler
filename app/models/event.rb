cb7a68ae9bde8e24f73398ee96d7cb90786b716b
David Jimenez <davidejimenezp@berkeley.edu>
2012-04-13 14:18:39 -0700

    added function to calculate number of nurses off

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,8 +9,6 @@ class Event < ActiveRecord::Base
 
   validates_with Rules
   
-  @@additional_months = [] # needs to be 1,2,3 and each month can be in here multiple times
-
   def self.additional_months
     return @@additional_months
   end
@@ -18,13 +16,6 @@ class Event < ActiveRecord::Base
   def self.all_display_columns
     ['start date', 'end date', 'Change vacation', '']
   end
-  
-  def self.calculate_length (event)
-    start_at = event.start_at.to_date
-    end_at = event.end_at.to_date
-    return days_total = end_at - start_at
-  end
-  
 
 # the way the event_calendar gem handles colors for events
 # on the calendar is with 'color' in the model. this sets the
