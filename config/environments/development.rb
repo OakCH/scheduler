ceb6bc4310963fb167e309c0cd9298752aaf7ff6
Sheel Choksi <sheelchoksi@berkeley.edu>
2012-04-13 17:38:17 -0700

    Emails should actually work in production now, disabled for test and development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Scheduler::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -15,6 +15,9 @@ Scheduler::Application.configure do
   
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+
+  # Don't let mailer send
+  config.action_mailer.perform_deliveries = false
   
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
