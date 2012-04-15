74bfc7a34fb6f8a00f4e0fe44edff2e3cca490ca
Andrew Nguyen <andrewnguyen@berkeley.edu>
2012-04-15 16:19:18 -0700

    woops regarding contexts

--- spec/controllers/unit_controller_spec.rb
+++ spec/controllers/unit_controller_spec.rb
@@ -46,55 +46,52 @@ describe UnitController do
   end
 
   describe "edit" do
-
-    context "succesfully edit unit" do
-      it 'should call find from Unit' do
-        Unit.should_receive(:find_by_id).and_return(@unit1)
-        get :edit, :id => @unit1.id
-      end
-
       it 'should assign the given Unit' do
         get :edit, :id => @unit1
         assigns(:unit).id = @unit1.id
       end
-    end
 
-    context "fail to edit unit" do
-
-      it 'should flash an error if invalid id' do
+    it 'should flash an error if invalid id' do
         get :edit, :id => 'asdfasdfadf'
         flash[:error].should_not be_empty
       end
-
-      it 'should fail when you edit the name to another name' do
-        name = @unit1.name
-        get :edit, :id => @unit2.id
-        flash[:error].should == "Unit name taken"
-      end
-
-    end
-
   end
 
   describe "update" do
-    it 'should flash an error if invalid id' do
-      put :update, :id => 'asdfasdfadf'
-      flash[:error].should_not be_empty
+    context "succesfully edit unit" do
+      it 'should call find from Unit' do
+        Unit.should_receive(:find_by_id).and_return(@unit1)
+        put :update, :id => @unit1.id
+      end
+
+      it 'should change the name of a unit' do
+        new_name = "ICU"
+        id = @unit1.id
+        put :update, :id => id, :new_name => new_name
+        unit = Unit.find_by_id(id)
+        unit.name.should == new_name
+      end
+
+      it 'should redirect to unit' do
+        put :update, :id => @unit1.id, :new_name => "sasafrass"
+        response.should redirect_to(unit_path)
+      end
     end
 
-    it 'should change the name of a unit' do
-      new_name = "ICU"
-      id = @unit1.id
-      put :update, :id => id, :new_name => new_name
-      unit = Unit.find_by_id(id)
-      unit.name.should == new_name
-    end
+    context "fail to edit unit" do
+      it 'should fail when you edit the name to another name' do
+        name = @unit1.name
+        put :update, :id => @unit2.id
+        flash[:error].should == "Unit name taken"
+      end
 
-    it 'should redirect to unit' do
-      put :update, :id => @unit1.id, :new_name => "sasafrass"
-      response.should redirect_to(unit_path)
+      it 'should flash an error if invalid id' do
+        put :update, :id => 'asdfasdfadf'
+        flash[:error].should_not be_empty
+      end
     end
   end
+
 
   describe 'Destroy' do
     it 'should call find from unit' do
