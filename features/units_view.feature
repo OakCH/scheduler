5c1e472812f6d5db40b44db4718eafaf6651663f
Andrew Nguyen <andrewnguyen@berkeley.edu>
2012-04-15 16:00:10 -0700

    made fixes per comments

--- features/units_view.feature
+++ features/units_view.feature
@@ -6,7 +6,8 @@ So that I can have an up-to-date list of them.
 
 Background:
 
-  Given the following units exist
+  Given the following units exist:
+  | name |
   | Surgery |
   | Pediactrics |
 
@@ -15,6 +16,7 @@ Background:
 
 Scenario: Viewing list of units
   Then I should see "Surgery"
+  And I should see "Pediatrics"
 
 Scenario: Adding a unit
   When I follow "add units"
@@ -23,12 +25,24 @@ Scenario: Adding a unit
   Then I should see "Cardiology"
   And I should see "Surgery"
 
+Scenario: Adding an existing unit
+  When I follow "add units"
+  And I fill in "Unit Name" with "Surgery"
+  And I press "Submit"
+  Then I should see "Unit name taken"
+
 Scenario: Updating a unit
   When I follow "Surgery"
   And I fill in "Unit Name" with "Cardiology"
   And I press "Submit"
   Then I should see "Cardiology"
   And I should not see "Surgery"
+
+Scenario: Changing a name to an existing unit
+  When I follow "Surgery"
+  And I fill in "Unit Name" with "Pediatrics"
+  And I press "Submit"
+  Then I should see "Unit name taken"
 
 Scenario: Deleting a unit
   When I follow "Surgery"
--- spec/controllers/unit_controller_spec.rb
+++ spec/controllers/unit_controller_spec.rb
@@ -7,7 +7,7 @@ describe UnitController do
     @units = [@unit1, @unit2]
   end
 
-  describe "Show" do
+  describe "Index" do
     it 'should call find all for units' do
       Unit.should_receive(:find).with(:all).and_return(@units)
       get :index
@@ -20,55 +20,78 @@ describe UnitController do
   end
 
   describe "Create" do
-    it 'should add a new unit' do
-      name = "ICU"
-      @new_unit = FactoryGirl.create(:unit, :name => name)
-      post :create, :name => name
-      found_unit = Unit.find_by_name(name)
-      found_unit.should == @new_unit
+    context "Successfully add a new unit" do
+      it 'should add a new unit' do
+        name = "ICU"
+        @new_unit = FactoryGirl.create(:unit, :name => name)
+        post :create, :name => name
+        found_unit = Unit.find_by_name(name)
+        found_unit.should == @new_unit
+      end
+
+      it 'should redirect to the index' do
+        name = "ICU"
+        post :create, :name => name
+        response.should redirect_to(unit_path)
+      end
     end
 
-    it 'should redirect to the index' do
-      name = "ICU"
-      post :create, :name => name
-      response.should redirect_to(unit_path)
+    context "Fail to add a new unit" do
+      it 'should not update if there is a taken name' do
+        name = @unit1.name
+        post :create, :name => name
+        flash[:error].should == "Unit name taken."
+      end
     end
   end
 
   describe "edit" do
-    it 'should call find from Unit' do
-      Unit.should_receive(:find_by_id).and_return(@unit1)
-      get :edit, :unit_id => @unit1.id
+
+    context "succesfully edit unit" do
+      it 'should call find from Unit' do
+        Unit.should_receive(:find_by_id).and_return(@unit1)
+        get :edit, :id => @unit1.id
+      end
+
+      it 'should assign the given Unit' do
+        get :edit, :id => @unit1
+        assigns(:unit).id = @unit1.id
+      end
     end
 
-    it 'should assign the given Unit' do
-      get :edit, :unit_id => @unit1
-      assigns(:unit).id = @unit1.id
-    end
+    context "fail to edit unit" do
 
-    it 'should flash an error if invalid id' do
-      get :edit, :unit_id => 'asdfasdfadf'
-      flash[:error].should_not be_empty
+      it 'should flash an error if invalid id' do
+        get :edit, :id => 'asdfasdfadf'
+        flash[:error].should_not be_empty
+      end
+
+      it 'should fail when you edit the name to another name' do
+        name = @unit1.name
+        get :edit, :id => @unit2.id
+        flash[:error].should == "Unit name taken"
+      end
+
     end
 
   end
 
   describe "update" do
     it 'should flash an error if invalid id' do
-      put :update, :unit_id => 'asdfasdfadf'
+      put :update, :id => 'asdfasdfadf'
       flash[:error].should_not be_empty
     end
 
     it 'should change the name of a unit' do
       new_name = "ICU"
       id = @unit1.id
-      put :update, :unit_id => id, :new_name => new_name
+      put :update, :id => id, :new_name => new_name
       unit = Unit.find_by_id(id)
       unit.name.should == new_name
     end
 
     it 'should redirect to unit' do
-      put :update, :unit_id => @unit1.id, :new_name => "sasafrass"
+      put :update, :id => @unit1.id, :new_name => "sasafrass"
       response.should redirect_to(unit_path)
     end
   end
@@ -76,22 +99,22 @@ describe UnitController do
   describe 'Destroy' do
     it 'should call find from unit' do
       Unit.should_receive(:find_by_id).and_return(@unit1)
-      delete :destroy, :unit_id => @unit1.id
+      delete :destroy, :id => @unit1.id
     end
 
     it 'should flash if there is an invalid unit' do
-      delete :destroy, :unit_id => "sasafrass"
+      delete :destroy, :id => "sasafrass"
       flash[:error].should_not be_empty
     end
 
     it 'should call destroy on unit' do
       Unit.stub(:find_by_id).and_return(@unit1)
       @unit1.should_receive(:destroy)
-      delete :destroy, :unit_id => @unit1.id
+      delete :destroy, :id => @unit1.id
     end
 
     it 'should redirect' do
-      delete :destroy, :unit_id => @unit1.id
+      delete :destroy, :id => @unit1.id
       response.should redirect_to(unit_path)
     end
   end
