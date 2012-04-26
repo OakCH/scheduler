6cc12b5dbfef8877c0f70a4cc699b11e8c859c91
Sheel Choksi <sheelchoksi@berkeley.edu>
2012-04-26 00:40:50 -0700

    Adding in password validations for user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   
-  devise :invitable, :database_authenticatable, :recoverable, :rememberable#, :validatable (to be included when invitations are implemented)
+  devise :invitable, :database_authenticatable, :recoverable, :rememberable
+  
+  validates_format_of :email, :with => /\A[^@]+@[^@]+\z/, :allow_blank => true
+  
+  validates_presence_of :password, :if => :not_new_record?
+  validates_length_of :password, :within => 6..128, :allow_blank => true, :if => :not_new_record?
   
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
@@ -8,5 +13,9 @@ class User < ActiveRecord::Base
   
   validates_presence_of :name, :email
   validates_uniqueness_of :email
+  
+  def not_new_record?
+    !new_record?
+  end
   
 end
