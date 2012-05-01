959db31d5f93a888d1c77de99a4ea05b136e77e8
Andrew Nguyen <andrewnguyen@berkeley.edu>
2012-04-30 20:58:16 -0700

    admin units story pt 2

---
+++ app/controllers/admin_unit_controller.rb
@@ -0,0 +1,37 @@
+class AdminUnitController < ApplicationController
+
+  before_filter :authenticate_admin!
+
+  def index
+    @units = Unit.find(:all)
+    puts @units.map { |a| a.name }
+    @admin_units = current_admin.units
+  end
+
+  def update
+    unit_names = params[:units]
+
+    unit_names ||= []
+
+    # Find units with name given
+    units = []
+    unit_names.each do | name, val |
+      unit = Unit.find_by_name(name)
+      if not unit
+        flash[:error] = "One or more units could not be added or removed."
+        break
+      elsif val
+        units << unit
+      end
+    end
+
+    current_admin.units = units
+
+    if not current_admin.save
+      flash[:error] += "Failed to change units."
+    end
+
+    redirect_to(admins_units_index_path)
+  end
+
+end
