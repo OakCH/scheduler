e8d22097c520c8c179715ece4cf3ff294f61745d
Christine Lu <christinelu.08@gmail.com>
2012-05-01 22:57:56 -0700

    fixed a failing rspec case. dont know what the bug was. at least its gone now!

--- app/validators/rules.rb
+++ app/validators/rules.rb
@@ -132,18 +132,13 @@ class Rules < ActiveModel::Validator
   end
 
   def less_than_max_per_day?(record)
-    if Event.find_by_id(record.id)
-      update = 1 #true
-    else
-      update = 0 #false
-    end
     @shift = record.nurse.shift
     @unit_id = record.nurse.unit.id
     @max_per = record.nurse.unit.calculate_max_per_day(@unit_id, @shift)
     start_date = record.start_at.to_date
     end_date = record.end_at.to_date
     while start_date <= end_date do
-      @num_on_this_day = num_nurses_on_day(start_date, @shift, @unit_id, update)
+      @num_on_this_day = num_nurses_on_day(start_date, @shift, @unit_id, record.id)
       if @num_on_this_day < @max_per[:year]
         start_date = start_date.next_day
       elsif less_than_max_in_additional_month?(start_date)
@@ -179,7 +174,7 @@ class Rules < ActiveModel::Validator
     @shift = record.nurse.shift
     holiday_limit = UnitAndShift.get_holiday_obj(@unit, @shift)
     if holiday_limit != nil && during_holidays(record)# no holiday restriction
-      num_today = num_nurses_on_day(record.start_at, @shift, @unit, update)
+      num_today = num_nurses_on_day(record.start_at, @shift, @unit, record.id)
       return num_today < holiday_limit.holiday
     else return true
     end
@@ -197,7 +192,7 @@ class Rules < ActiveModel::Validator
     return @rtn_months
   end
     
-  def num_nurses_on_day(start_date, shift, unit_id, update)
+  def num_nurses_on_day(start_date, shift, unit_id, id)
     max_weeks = 6 
     range_buffer = (max_weeks * 7) + 1
 
@@ -212,12 +207,16 @@ class Rules < ActiveModel::Validator
 
     num_nurses = 0 
     events.each do |e|
-    if (e.start_at.to_date <= start_date) and (start_date <= e.end_at.to_date)
-          num_nurses += 1
-        end
+      if e.id == id
+        next
+      end
+      
+      if (e.start_at.to_date <= start_date) and (start_date <= e.end_at.to_date)
+        num_nurses += 1
+      end
     end
 
-    return num_nurses - update
+    return num_nurses
   end
 
   def calculate_length (event)
--- spec/validators/rules_spec.rb
+++ spec/validators/rules_spec.rb
@@ -168,61 +168,66 @@ describe Rules do
 
       @nurse1 = @nurses_unit1[0]
       @nurse2 = @nurses_unit1[1]
-
       @nurse3 = @nurses_unit2[0]
 
       # event from 4/4/2012 to 4/19/2012 for nurse1
-      FactoryGirl.create(:event, :name => 'e1', :start_at => DateTime.new(2012,4,4,0,0,0), :end_at => DateTime.new(2012,4,19,0,0,0), :nurse_id => @nurse1.id)
+      FactoryGirl.create(:event, :id => 1, :name => 'e1', :start_at => DateTime.new(2012,4,4,0,0,0), :end_at => DateTime.new(2012,4,19,0,0,0), :nurse_id => @nurse1.id)
 
       # event from 4/10/2012 to 4/20/2012 for nurse2
       FactoryGirl.create(:event, :name => 'e2', :start_at => DateTime.new(2012,4,10,0,0,0), :end_at => DateTime.new(2012,4,20,0,0,0), :nurse_id => @nurse2.id)
 
       # event from 4/11/2012 to 4/21/2012 for nurse3
       FactoryGirl.create(:event, :name => 'e3', :start_at => DateTime.new(2012,4,11,0,0,0), :end_at => DateTime.new(2012,4,21,0,0,0), :nurse_id => @nurse3.id)
+    
     end
 
     it 'should check the number of events on April 4 for unit1, PMs' do
       rules = Rules.new(nil)
       test_date =  DateTime.new(2012,4,4,0,0,0)
-      rules.num_nurses_on_day(test_date, @shift1, @unit1.id, 0).should == 1
+      rules.num_nurses_on_day(test_date, @shift1, @unit1.id, nil).should == 1
     end
 
     it 'should check the number of events on April 10 for unit1, PMs' do
       rules = Rules.new(nil)
       test_date = DateTime.new(2012,4,10,0,0,0)
-      rules.num_nurses_on_day(test_date, @shift1,  @unit1.id, 0).should == 2
+      rules.num_nurses_on_day(test_date, @shift1,  @unit1.id, nil).should == 2
     end
 
     it 'should check the number of events on April 21 for unit1, PMs' do
       rules = Rules.new(nil)
       test_date = DateTime.new(2012,4,21,0,0,0)
-      rules.num_nurses_on_day(test_date, @shift1, @unit1.id, 0).should == 0
+      rules.num_nurses_on_day(test_date, @shift1, @unit1.id, nil).should == 0
     end
 
     it 'should check the number of events on April 4 for unit2, PMs' do
       rules = Rules.new(nil)
       test_date =  DateTime.new(2012,4,4,0,0,0)
-      rules.num_nurses_on_day(test_date, @shift1, @unit2.id, 0).should == 0
+      rules.num_nurses_on_day(test_date, @shift1, @unit2.id, nil).should == 0
     end
 
     it 'should check the number of events on April 10 for unit2, PMs' do
       rules = Rules.new(nil)
       test_date = DateTime.new(2012,4,10,0,0,0)
-      rules.num_nurses_on_day(test_date, @shift1,  @unit2.id, 0).should == 0
+      rules.num_nurses_on_day(test_date, @shift1,  @unit2.id, nil).should == 0
     end
 
     it 'should check the number of events on April 21 for unit2, PMs' do
       rules = Rules.new(nil)
       test_date = DateTime.new(2012,4,21,0,0,0)
-      rules.num_nurses_on_day(test_date, @shift1, @unit2.id, 0).should == 1
+      rules.num_nurses_on_day(test_date, @shift1, @unit2.id, nil).should == 1
     end
 
     it 'should check the number of events on April 22 for unit2, PMs' do
       rules = Rules.new(nil)
       test_date = DateTime.new(2012,4,22,0,0,0)
-      rules.num_nurses_on_day(test_date, @shift1, @unit2.id, 0).should == 0
+      rules.num_nurses_on_day(test_date, @shift1, @unit2.id, nil).should == 0
     end
 
+    it 'should check the number of events on April 4 for unit1, PMs if considered an update' do
+      rules = Rules.new(nil)
+      test_date =  DateTime.new(2012,4,4,0,0,0)
+      rules.num_nurses_on_day(test_date, @shift1, @unit1.id, 1).should == 0
+    end
   end
 
   subject {Validatable.new}
@@ -263,8 +268,6 @@ describe Rules do
       @nurse_id = @nurse.id
       subject.stub(:nurse_id).and_return(@nurse_id)
       subject.stub(:nurse).and_return(@nurse)
-      subject.stub(:id).and_return(nil)
-      subject.stub(:pto).and_return(false)
       # add 2 weeks of scheduled vacation into nurse
       @event1 = FactoryGirl.create(:event, :start_at => DateTime.new(2012,3,4,0,0,0), :end_at => DateTime.new(2012,3,10,0,0,0))
       @event2 = FactoryGirl.create(:event, :start_at => DateTime.new(2012,4,4,0,0,0), :end_at => DateTime.new(2012,4,10,0,0,0))
@@ -273,6 +276,8 @@ describe Rules do
       # this is the third week currently being validated
       subject.stub(:start_at).and_return(DateTime.new(2012,5,4,0,0,0))
       subject.stub(:end_at).and_return(DateTime.new(2012,5,10,0,0,0))
+      subject.stub(:id).and_return(nil)
+      subject.stub(:pto).and_return(false)
     end
     it 'should return true if taken 21 vacation days and have 28' do
       @nurse.num_weeks_off = 4
@@ -295,8 +300,6 @@ describe Rules do
       @nurse_id = @nurse.id
       subject.stub(:nurse_id).and_return(@nurse_id)
       subject.stub(:nurse).and_return(@nurse)
-      subject.stub(:id).and_return(nil)
-      subject.stub(:pto).and_return(false)
       # make sure @@max_segs = 4
       # add 3 segs in
       @nurse.num_weeks_off = 5
@@ -306,6 +309,8 @@ describe Rules do
       @nurse.events << @event2
       subject.stub(:start_at).and_return(DateTime.new(2012,5,4,0,0,0))
       subject.stub(:end_at).and_return(DateTime.new(2012,5,10,0,0,0))
+      subject.stub(:id).and_return(nil)
+      subject.stub(:pto).and_return(false)
     end
     it 'should return true if 3 segs out of 4' do
       subject.should be_valid
@@ -425,7 +430,8 @@ describe Rules do
   describe 'checking overlaps?' do
     before(:each) do
       @nurse = FactoryGirl.create(:nurse, :unit => @unit)
-      @event = FactoryGirl.create(:event, :nurse_id => @nurse.id)
+      @event = FactoryGirl.create(:event, :start_at => DateTime.new(2012,3,5,0,0,0), :end_at => DateTime.new(2012,3,11,0,0,0))
+      @nurse.events << @event
     end
     it 'should not allow overlapping vacations' do
       @new_event = FactoryGirl.create(:event, :start_at => DateTime.new(2012,3,7,0,0,0), :end_at => DateTime.new(2012,3,13,0,0,0))
@@ -433,7 +439,7 @@ describe Rules do
       subject.stub(:end_at).and_return(@new_event.end_at)
       subject.stub(:nurse).and_return(@nurse)
       subject.stub(:nurse_id).and_return(@nurse.id)
-      subject.stub(:id).and_return(@new_event.id)
+      subject.stub(:id).and_return(nil)
       subject.stub(:pto).and_return(false)
       subject.should have(1).error_on(:overlap)
     end
@@ -443,7 +449,7 @@ describe Rules do
       subject.stub(:end_at).and_return(@new_event.end_at)
       subject.stub(:nurse).and_return(@nurse)
       subject.stub(:nurse_id).and_return(@nurse.id)
-      subject.stub(:id).and_return(@new_event.id)
+      subject.stub(:id).and_return(nil)
       subject.stub(:pto).and_return(false)
       subject.should be_valid
     end
@@ -460,7 +466,7 @@ describe Rules do
       subject.stub(:end_at).and_return(@new_event.end_at)
       subject.stub(:nurse).and_return(@nurse)
       subject.stub(:nurse_id).and_return(@nurse.id)
-      subject.stub(:id).and_return(@new_event.id)
+      subject.stub(:id).and_return(nil)
       subject.stub(:pto).and_return(false)
       subject.should be_valid
     end
@@ -471,7 +477,7 @@ describe Rules do
       subject.stub(:end_at).and_return(@new_event.end_at)
       subject.stub(:nurse).and_return(@nurse)
       subject.stub(:nurse_id).and_return(@nurse.id)
-      subject.stub(:id).and_return(@new_event.id)
+      subject.stub(:id).and_return(nil)
       subject.stub(:pto).and_return(false)
       subject.should be_valid
     end
