9c128f7c5835a464ac3b40d0fe86e31142bd02e3
Christine Lu <christinelu.08@gmail.com>
2012-05-02 18:20:55 -0700

    current_year.feature now runs green. adding in support if no current_year already exists

--- app/controllers/current_year_controller.rb
+++ app/controllers/current_year_controller.rb
@@ -3,19 +3,25 @@ class CurrentYearController < ApplicationController
   before_filter :authenticate_admin!
 
   def index
-    @current_year = CurrentYear.find(:all).first
+    if CurrentYear.first
+      @current_year = CurrentYear.first.year
+    end
   end
 
   def update
     # hack hack hack hack
     new_year = params[:year]
-    current_year = CurrentYear.find(:all)
+    current_year = CurrentYear.first
     begin
-      current_year.first.year = Integer(new_year)
+      if current_year
+        current_year.year = Integer(new_year)
+      else
+        current_year = CurrentYear.new(:year => new_year)
+      end
     rescue
       flash[:error] = "Invalid year entered"
     end
-    unless current_year[0].save
+    unless current_year.save
       flash[:error] = "Problem changing current year."
     end
     redirect_to(current_year_index_path)
--- features/step_definitions/current_year_steps.rb
+++ features/step_definitions/current_year_steps.rb
@@ -1,7 +1,13 @@
 Given /the following current years exist/ do |year_table|
   year_table.hashes.each do |year_params|
     year = year_params[:year]
-    FactoryGirl.create(:current_year, :year => year)
+    record = CurrentYear.first
+    if record
+      record.year = year
+      record.save
+    else
+      FactoryGirl.create(:current_year, :year => year)
+    end
   end
 end
 
